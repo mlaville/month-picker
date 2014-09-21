bc415c713c3f62c4bf9c38bd418cc870a34b2f11
vava <marc.laville@polinux.net>
2014-09-21 22:08:42 +0200

    restitution de la valeur du input

--- js/monthPicker.js
+++ js/monthPicker.js
@@ -9,9 +9,7 @@
  *
  * un month-picker pur Javascript
  * 
- *
- * A Faire
- * -position du tip ( west, east, north, south )
+ * @date revision 21/09/2014 restitue la valeur du champ texr dans le panneau de saisie
  *
  * Licensed under the GPL license:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -37,7 +35,7 @@ var monthPickerFactory = (function ( document ) {
 		 */
 		monthPicker =  function ( inputElt, options ) {
 			var valeur = inputElt.value,
-				tabVal = valeur.split('/'), // Voir un regex pour généraliser la construction 
+				tabVal = valeur.toIntArray(), // Voir un regex pour généraliser la construction 
 				b_mp = document.createElement('b'),
 				span_mp = b_mp.appendChild( document.createElement('span') ),
 				slct_year = span_mp.appendChild( document.createElement('select') ),
@@ -65,12 +63,14 @@ var monthPickerFactory = (function ( document ) {
 				},			
 				selectAnnee = function( e ) {
 					tabVal[1] = slct_year.value;
-					inputElt.value = '' + tabVal[0]  + '/' + tabVal[1];
+					inputElt.value = tabVal.join('/');
 				},
 				fillSelect = function( an ) {
 					an = an || (new Date()).getFullYear();
 					for(var i = an - 3 ; i < an + 10 ; i++) {
 						slct_year.appendChild( document.createElement('option') ).textContent = i;
+						
+						slct_year.lastChild.setAttribute( 'value', i );
 						if( an == i ) {
 							slct_year.lastChild.setAttribute( 'selected', 'selected' );
 						}
@@ -78,7 +78,7 @@ var monthPickerFactory = (function ( document ) {
 					slct_year.addEventListener( 'change', selectAnnee );
 					
 				},
-				fillMois = function( mois, tabMois ) {
+				fillMois = function( mois, nom, tabMois ) {
 					var ulMois = document.createElement('ul');
 					
 					tabMois = tabMois || Date.monthNames().map( function(str) { return ( str.length > 4 ) ? str.substr(0 , 3) + '.' : str; } );
@@ -90,7 +90,7 @@ var monthPickerFactory = (function ( document ) {
 							
 						rd_mois.value = i + 1;
 						rd_mois.setAttribute( 'type', 'radio');
-						rd_mois.setAttribute( 'name', 'mois');
+						rd_mois.setAttribute( 'name', 'mois-' + nom );
 						rd_mois.addEventListener( 'change', clickBtnMois );
 						rd_mois.checked = ( mois == i + 1 );
 						if( rd_mois.value == '' && mois == i + 1 ) {
@@ -104,6 +104,22 @@ var monthPickerFactory = (function ( document ) {
 					}
 					
 					return span_mp.appendChild( ulMois );
+				},
+				/*
+				 * Restitue la valeur de l'input sur la panneau
+				 */
+				setValue = function( str ) {
+					var tabVal = str.toIntArray();
+					
+					radios = span_mp.querySelectorAll('ul li input');
+					for( var i = 0 ; i < radios.length ; i++ ) {
+						var rd = radios[i];
+						
+						if( rd.value == tabVal[0] ) {
+							rd.checked = true;
+						}
+					}
+					slct_year.value = tabVal[1];
 				};
 				
 			b_mp.classList.add( 'month-picker' );
@@ -111,10 +127,14 @@ var monthPickerFactory = (function ( document ) {
 			inputElt.classList.add( 'month-picker' );
 			fillSelect( ( tabVal.length > 1 ) ? parseInt( tabVal[1] ) : null );
 			span_mp.appendChild( document.createElement('br') );
-			fillMois( ( tabVal.length ) ? parseInt( tabVal[0] ) : null );
+			fillMois( ( tabVal.length ) ? parseInt( tabVal[0] ) : null , inputElt.getAttribute('name') );
 			
 			inputElt.parentNode.insertBefore(b_mp, inputElt);
 			b_mp.insertBefore(inputElt, span_mp);
+			setValue( inputElt.value )
+			inputElt.addEventListener( 'change', function( e ) {
+				return setValue( e.target.value ) ;
+			});
 			
 			return b_mp;
 		};
